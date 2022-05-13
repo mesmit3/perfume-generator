2e8921ac6104b2410dba7d0414f6d92371c7c5fa
mesmit3 <50841000+mesmit3@users.noreply.github.com>
2022-05-13 15:41:34 -0600

    Update Perfume.py

--- Perfume.py
+++ Perfume.py
@@ -2,10 +2,10 @@
 import random
 
 list_hex = ["DTF", "Would Thou Like the Taste of Butter", "Loup-Garou", "Bah, Humbug!", "The Bell Witch", "Brightening the Daybreak", "Embalming Fluid", "Laudanum", "Just Took a DNA Test", "Laundromat", "Leopardite", "Smoky Quartz", "Trashcan Man", "Waking the Witch", "Graveyard", "Thè Noir", "That Fancy Wine", "Tomatè", "Ambre d'Or", "Blue Moon", "Unbaptism", "Bisou", "Witchboard", "Alexandrite", "Cetalox", "Desert Rain", "Fluorite", "Hexenhaus", "Pearanormal Activity", "Reefer Madness", "l'air en hiver", "Le Chat Noir", "Hecate", "Norman Loves Mother", "Thanatos", "Krampusnacht", "Elemental", "Mephistopheles", "White Amber", "Necromancer"]
-list_pine = ["Auntumnal", "Eldritch", "Apple Tabac", "Fanghorn", "Glühwein", "oxylus", "Bindebole", "Steading", "Boreal", "Murkwood", "Christmas Wine"]
+list_pine = ["Auntumnal", "Eldritch", "Apple Tabac", "Fanghorn", "Glühwein", "oxylus", "Bindebole", "Steading", "Boreal", "Murkwood", "Christmas Wine", "Chandlery", "Brokilän", "Icefall"]
 list_cocoapink = ["Opera Ghost", "Handbook for the Recently Deceased", "Fauno Obscuro", "Beyond the Goblin City"]
 list_astrid = ["Merci no. 29", "the creek flows over me", "when dawn breaks", "basking with lizards", "rattlesnake and hummingbird", "flowers growing"]
-list_other = ["Rise", "couer de grenade", "Love Dart", "Smokeable", "Ostara", "Baculum", "The Huntress", "Tramp", "Trollop", "Jezebel", "Grass", "Redcorder Music", "The One Lascivious", "Within a Daze of Passion", "Mercury Retrograde", "Yennefer"] 
+list_other = ["Rise", "couer de grenade", "Love Dart", "Smokeable", "Ostara", "Baculum", "Bawd", "The Huntress", "Tramp", "Trollop", "Jezebel", "Grass", "Redcorder Music", "The One Lascivious", "Within a Daze of Passion", "Mercury Retrograde", "Yennefer"] 
 
 seqs = list_hex, list_pine, list_cocoapink, list_astrid, list_other
 
